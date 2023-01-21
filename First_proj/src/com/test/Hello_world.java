c0f65d27d49be5b340ecaaf97624d06ff845c55a
Basaveshw;Arjun <basavarajw96@gmail.com;mvgurikar025@gmail.com>
2023-01-22 03:55:53 +0530

    1st comitt to git

---
+++ First_proj/src/com/test/Hello_world.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class Hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to new project for testing..!");
+
+	}
+
+}
