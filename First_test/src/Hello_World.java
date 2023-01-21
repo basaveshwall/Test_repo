a9cf25a8b24c2c594b0e97a82e076cfb1c60d5f3
Basaveshw;Arjun <basavarajw96@gmail.com;mvgurikar025@gmail.com>
2023-01-21 17:34:21 +0530

    This is the first commit to github

---
+++ First_test/src/Hello_World.java
@@ -0,0 +1,11 @@
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is for our testing for devops prject....with Hello world to java projects");
+
+	}
+
+}
